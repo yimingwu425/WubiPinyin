e8c3cc9ffa4f7d7af516842271cc38b394fdedaf
YIMING WU <yiming.wu425@gmail.com>
2026-07-26 21:20:41 +0800

    fix: force complete WinUI projections

---
+++ WubiPinyinSettings/WinUIProjectIncludes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <unknwn.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Interop.h>
+#include <winrt/Microsoft.UI.Xaml.Markup.h>
+#include <winrt/Microsoft.UI.Xaml.Media.h>
+#include <winrt/Microsoft.UI.Xaml.Navigation.h>
+#include <winrt/WubiPinyinSettings.h>
