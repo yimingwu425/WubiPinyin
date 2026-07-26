f66f1b0e65c9eee2fd7b906382c68d45ff13c6e1
YIMING WU <yiming.wu425@gmail.com>
2026-07-26 22:28:42 +0800

    fix: provide XAML metadata include context

---
+++ WubiPinyinSettings/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "WinUIProjectIncludes.h"
+#include "MainWindow.xaml.h"
+#include "Pages/AboutPage.xaml.h"
+#include "Pages/AppearancePage.xaml.h"
+#include "Pages/DictionaryPage.xaml.h"
+#include "Pages/InputPage.xaml.h"
+#include "Pages/LearningPage.xaml.h"
