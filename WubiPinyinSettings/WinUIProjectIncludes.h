005ed911bee981ec70a556362ef2d5f65067374c
YIMING WU <yiming.wu425@gmail.com>
2026-07-26 22:17:15 +0800

    fix: include XAML type name projection

--- WubiPinyinSettings/WinUIProjectIncludes.h
+++ WubiPinyinSettings/WinUIProjectIncludes.h
@@ -6,6 +6,7 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.UI.Xaml.Interop.h>
 #include <winrt/Microsoft.UI.Xaml.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
